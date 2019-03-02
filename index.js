b78aea247607049e37a5d0f85103a0cf0180c92c
Philip Manijak <github@morningshift.org>
2019-03-01 23:13:23 -0800

    Endpoint: Get messages from the database

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+require('dotenv').config();
+
+const dateformat = require('dateformat');
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+const { Pool, Client } = require('pg')
+const connectionString = process.env.POSTGRESQL_MESSAGES;
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+async function get(ctx) {
+   const client = new Client({ connectionString });
+   client.connect();
+
+   var lastWeek = dateformat(addDays(new Date(), -7), 'yyyy-mm-dd');
+
+   try {
+      const messages = `
+         SELECT data 
+           FROM messages
+          where data->>'endDate' > '${lastWeek}'`;
+
+      var res = await client.query(messages);
+      client.end();
+
+      ctx.status = 200;
+      ctx.body = res.rows;
+   }
+   catch (err) {
+      client.end();
+      ctx.throw(err);
+   }
+}
+
+app.use(get);
 
 app.listen(process.env.PORT || 5000);
